e9fffc314e27f1941fdd3421ed427a278d466a87
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-02-10 15:50:20 +0100

    level 12

---
+++ code/code_level12.py
@@ -0,0 +1,26 @@
+from main import move_left, move_right, move_up, move_down, get_position, is_no_wall_down, is_not_at_goal, is_no_wall_up
+
+
+# This should be created by the schnuppi
+def go_down():
+    while is_no_wall_down():
+        move_down()
+
+def go_up():
+    while is_no_wall_up():
+        move_up()
+
+
+def main():
+
+    # Do this by only using while loops and also don't repeat code to go down and up, even if they are different lengths
+
+    while is_not_at_goal():
+        if is_no_wall_down():
+            go_down()
+            go_up()
+            move_right()
+        else:
+            move_right()
+
+    return
