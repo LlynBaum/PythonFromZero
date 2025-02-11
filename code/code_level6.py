6e8033014ffe4447c18f126af561a1ba2d07bc21
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-02-11 16:08:45 +0100

    Update code_level6.py

--- code/code_level6.py
+++ code/code_level6.py
@@ -1,13 +1,8 @@
-from main import move_left, move_right, move_up, move_down, get_position, is_not_at_goal
+from main import move_left, move_right, move_up, move_down, is_not_at_goal
 
 
 def main():
 
-    # Now you have to travel a long distance in two directions. Maybe you can modify the code from last
-    # Level to also travel in another direction
-
-    while is_not_at_goal():
-        move_down()
-        move_right()
+    # hier kommt dein code
 
     return
