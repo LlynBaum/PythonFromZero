aaea2d0dac0cd066b9ff2645cd7ecedbfd697918
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-02-11 16:09:25 +0100

    Update code_level8.py

--- code/code_level8.py
+++ code/code_level8.py
@@ -1,15 +1,8 @@
-from main import move_left, move_right, move_up, move_down, get_position, is_wall_left, is_no_wall_left, is_not_at_goal
+from main import move_left, move_right, move_up, move_down, is_no_wall_left, is_not_at_goal
 
 
 def main():
 
-    # that is a lot of repetition, this can be simplified by using a loop and a condition
-    while is_not_at_goal():
-        if is_no_wall_left():
-            move_left()
-            move_right()
-            move_down()
-        else:
-            move_down()
+    # hier kommt dein code
 
     return
