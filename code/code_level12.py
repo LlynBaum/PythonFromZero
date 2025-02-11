79041f741e79606a2ca2df70c5538bcee512fdf3
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-02-11 16:12:32 +0100

    Update code_level12.py

--- code/code_level12.py
+++ code/code_level12.py
@@ -1,21 +1,8 @@
-from main import move_left, move_right, move_up, move_down, get_position, is_no_wall_down, is_not_at_goal, is_no_wall_up
-
-
-# This should be created by the schnuppi
-def pick_up_coin():
-    while is_no_wall_down():
-        move_down()
-    while is_no_wall_up():
-        move_up()
+from main import move_left, move_right, move_up, move_down, is_no_wall_down, is_not_at_goal, is_no_wall_up
 
 
 def main():
 
-    # Do this without using any loops, except while is_not_at_goal() but as little code repetition as possible
+    # hier kommt dein code
 
-    while is_not_at_goal():
-        if is_no_wall_down():
-            pick_up_coin()
-            move_right()
-        else:
-            move_right()
+    return
