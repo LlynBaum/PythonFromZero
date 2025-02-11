c9161a371cc5198a3b44dfc10b7c5adeb88cd684
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-02-11 16:09:49 +0100

    Update code_level9.py

--- code/code_level9.py
+++ code/code_level9.py
@@ -1,20 +1,8 @@
-from main import move_left, move_right, move_up, move_down, get_position, is_no_wall_right, is_no_wall_left, \
-    is_not_at_goal
+from main import move_left, move_right, move_up, move_down, is_no_wall_right, is_no_wall_left, is_not_at_goal
 
 
 def main():
 
-    # the same as level 7 but this time we can also go to the other side
-    while is_not_at_goal():
-        if is_no_wall_left():
-            move_left()
-            move_right()
-            move_down()
-        elif is_no_wall_right():
-            move_right()
-            move_left()
-            move_down()
-        else:
-            move_down()
+    # hier kommt dein code
 
     return
