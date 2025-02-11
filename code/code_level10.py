d27b303c7d240b28eeb289497e60eab335623ba6
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-02-11 16:10:32 +0100

    Update code_level10.py

--- code/code_level10.py
+++ code/code_level10.py
@@ -1,17 +1,10 @@
-from main import move_left, move_right, move_up, move_down, get_position, is_not_at_goal
-
-
-# This should be created by the schnuppi
-def move_diagonal():
-        move_down()
-        move_right()
-
+from main import move_left, move_right, move_up, move_down, is_not_at_goal
 
 def main():
-
-    # remember level5, maybe we can make it simple if we can create our own move method.
 
     while is_not_at_goal():
         move_diagonal()
 
     return
+
+# hier kommt dein code
