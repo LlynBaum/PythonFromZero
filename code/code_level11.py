f4a77e312d99dc13ce6e3606824b45ea3e88af97
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-02-11 16:11:37 +0100

    Update code_level11.py

--- code/code_level11.py
+++ code/code_level11.py
@@ -1,17 +1,7 @@
 from main import move_left, move_right, move_up, move_down, get_position, is_no_wall_down, is_not_at_goal
 
 
-# This should be created by the schnuppi
-def pick_up_coin():
-    for i in range(7):
-        move_down()
-    for i in range(7):
-        move_up()
-
-
 def main():
-
-    # Do this without using any loops, except while is_not_at_goal() but as little code repetition as possible
 
     while is_not_at_goal():
         if is_no_wall_down():
@@ -19,3 +9,5 @@ def main():
             move_right()
         else:
             move_right()
+
+# hier kommt dein code
