dad68e3608a8be3821bf2354fef8c638ecceff37
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-02-11 16:07:26 +0100

    Update code_level3.py

--- code/code_level3.py
+++ code/code_level3.py
@@ -3,8 +3,6 @@ from main import move_left, move_right, move_up, move_down, get_position
 
 def main():
 
-    # Move the player to the exit sign, but make you life simpler with loops
-    for i in range(5):
-        move_down()
+    # hier kommt dein code
 
     return
