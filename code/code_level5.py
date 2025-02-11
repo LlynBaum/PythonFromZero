34f7db570cca7d62e7ed55f18e8f2935a92b9ad5
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-02-11 16:08:29 +0100

    Update code_level5.py

--- code/code_level5.py
+++ code/code_level5.py
@@ -1,11 +1,8 @@
-from main import move_left, move_right, move_up, move_down, get_position, is_not_at_goal
+from main import move_left, move_right, move_up, move_down, is_not_at_goal
 
 
 def main():
 
-    # That is a long way to walk, can this not be made easier? Counting is also annoying
-
-    while is_not_at_goal():
-        move_down()
+    # hier kommt dein code
 
     return
