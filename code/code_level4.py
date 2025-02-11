88648638f04add079bdeb1fd631bd07b7b727ec8
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-02-11 16:07:48 +0100

    Update code_level4.py

--- code/code_level4.py
+++ code/code_level4.py
@@ -3,10 +3,6 @@ from main import move_left, move_right, move_up, move_down, get_position
 
 def main():
 
-    # Move the player to the exit sign, but you can't walk through wooden boxes
-    move_down()
-    move_down()
-    move_right()
-    move_down()
+    # hier kommt dein code
 
     return
